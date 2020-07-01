8393ac798869d6835e0e0a97ad5d1b10f81a7690
AlekseyGribkov <Aleksey.V.Gribkov@rt.ru>
2020-07-01 20:42:00 +0300

    do not download folder build

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/haost/Desktop/temp/OTUS1
-# Build directory: /home/haost/Desktop/temp/OTUS1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_version "test_version")
